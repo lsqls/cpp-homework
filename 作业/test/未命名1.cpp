6b8b6f713f01b06505d3ca19696a690d81a79192
king <764666894@qq.com>
2017-10-19 11:42:28 +0800

    some adjustment

---
+++ 作业/test/未命名1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class t
+{
+	public:
+		t(int x);
+	private:
+	int a;
+	zi x;
+};
+class zi
+t::t(int x)
+{
+	a=x;
+}
+int main()
+{
+//	t arr[2]={t(1),t(2)};
+//	arr[0](1);
+//	arr[1](3);
+char t[]="abcdefg";
+char* p=&t;
+cout<<*p+3);
+
+
+int shuzu[3];
+shuzu[0]=1;
+shuzu[1]=
+	return 0;
+}
